2fc6395e9c142565bfbb1265618e211f1d909a73
6804062612242 chanasuk kongbasad <s6804062612242@email.kmutnb.ac.th>
2025-12-15 09:59:29 +0700

    lab3 +bonus

--- lab3 6.cpp
+++ lab3 6.cpp
@@ -2,10 +2,7 @@
 int main(){
 	int score,a;
 	scanf("%d",&score);
-	if(score > 100 || score < 0){
-		printf("error");
-	}
-	else if(score>=68){
+ 	if(score>=68){
 		if(score>=80){
 			printf("A\n");
 		}else if(score>=75){
